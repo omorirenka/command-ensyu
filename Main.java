16930324d3a274499676b0f13d822405aa4d3062
renka <r.oomori.sys22@morijyobi.ac.jp>
2022-11-08 10:14:23 +0900

    Main.java add program (commit change)

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main{
 	public static void main(String[] args){
-		double height = 1.6;
-		double weight = 60.0;
-		double bmi = ((double)Math.round(weight / Math.pow(height, 2)*10))/10;
-		System.out.println(bmi);
+		double height1 = 1.6;
+		double weight1 = 60.0;
+		double bmi1 = ((double)Math.round(weight1 / Math.pow(height1, 2)*10))/10;
+		System.out.println(bmi1);
+		double height2 = 1.5;
+		double weight2 = 56.0;
+		double bmi2 = ((double)Math.round(weight2 / Math.pow(height2, 2)*10))/10;
+		System.out.println(bmi2);
 	}
 }
